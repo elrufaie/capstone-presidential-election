6cb0740a2a297158e26e33c28dcf44d737e88eec
Harith Elrufaie <harith_elrufaie@intuit.com>
2020-07-21 23:48:44 -0700

    misc changes to fully dockerize and run on gcloud

--- app/main.py
+++ app/main.py
@@ -15,8 +15,13 @@ import time
 
 app = Flask(__name__)
 
+@app.route('/hello')
+def hello():
+    return 'hello world'
+
 @app.route("/")
 def chart():
+    print("calling / endpoint")
     #print("hello there", file=sys.stderr)
     #plot = emap.create_chart()
     #script, div = components(plot)
@@ -67,4 +72,6 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    print("loading app...")
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    # app.run(host='127.0.0.1', port=8080, debug=True)
