433855a72927a5a15970d8e8caa7c581085ef3c8
Harith Elrufaie <harith_elrufaie@intuit.com>
2020-07-24 20:04:18 -0700

    corrected port number

--- app/main.py
+++ app/main.py
@@ -102,5 +102,5 @@ if __name__ == '__main__':
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
     print("loading app...")
-    app.run(host="0.0.0.0", port=8555, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
     # app.run(host='127.0.0.1', port=8080, debug=True)
